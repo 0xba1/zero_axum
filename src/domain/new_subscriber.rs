c48c7e64c2fb24188941a2b19976088be7e2c77f
0xba1 <dev.boluwatife@gmail.com>
2022-12-06 08:43:36 +0100

    Chap 6

---
+++ src/domain/new_subscriber.rs
@@ -0,0 +1,19 @@
+use crate::routes::FormData;
+
+use super::{SubscriberEmail, SubscriberName};
+
+pub struct NewSubscriber {
+    pub email: SubscriberEmail,
+    pub name: SubscriberName,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+
+        Ok(Self { email, name })
+    }
+}
